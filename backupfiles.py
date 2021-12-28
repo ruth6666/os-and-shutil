2a284f529a56af5a58ceb6700cc5e640c0bb7b3d
ruth6666 <62940464+ruth6666@users.noreply.github.com>
2021-12-28 17:53:11 +0530

    Add files via upload

---
+++ backupfiles.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+source = input("Enter source folder name")
+destination = input("Enter destination folder name")
+source = source + "/"
+destination = destination + "/"
+listoffiles = os.listdir(source)
+for file in listoffiles:
+    shutil.copy((source+file),destination)
+    
